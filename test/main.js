802d39352b426062fbabe236d82aa09b48e264a9
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-26 09:29:22 -0700

    Move test entrypoint to test/

--- test/main.js
+++ test/main.js
@@ -10,11 +10,11 @@ karmaRunner.setupEnvironment();
 // Use webpack to include all app code _except_ the entry point so we can get
 // code coverage in the bundle, whether tested or not.
 // TODO: This doesn't seem to work.
-var srcReq = require.context('../../src', true, /\.js$/);
+var srcReq = require.context('../src', true, /\.js$/);
 srcReq.keys().map(srcReq);
 
 // Use webpack to infer and `require` tests automatically.
-var testsReq = require.context('../../src', true, /\.test.js$/);
+var testsReq = require.context('../src', true, /\.test.js$/);
 testsReq.keys().map(testsReq);
 
 karmaRunner.startKarma();
