2bc59e0a20edd8e5d0142d6fe20315997c7ab30f
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-26 09:15:42 -0700

    Move some test entrypoint logic to archetype

--- test/client/main.js
+++ test/client/main.js
@@ -1,27 +1,8 @@
-/**
- * Test setup for client-side tests.
- *
- * Intended for:
- * - Karma tests: `builder run test-frontend`
- * - Browser tests: `http://localhost:3000/test/client/test.html`
- */
-const chai = require('chai');
-const sinonChai = require('sinon-chai');
+/* eslint-disable import/default */
+/* global require:false */
+import karmaRunner from 'dash-components-archetype-dev/karma-runner';
 
-// --------------------------------------------------------------------------
-// Chai / Sinon / Mocha configuration.
-// --------------------------------------------------------------------------
-// Exports
-window.expect = chai.expect;
-
-// Plugins
-chai.use(sinonChai);
-
-// Mocha (part of static include).
-window.mocha.setup({
-  ui: 'bdd',
-  bail: false
-});
+karmaRunner.setupEnvironment();
 
 // --------------------------------------------------------------------------
 // Bootstrap
@@ -29,14 +10,11 @@ window.mocha.setup({
 // Use webpack to include all app code _except_ the entry point so we can get
 // code coverage in the bundle, whether tested or not.
 // TODO: This doesn't seem to work.
-const srcReq = require.context('../../src', true, /\.js$/);
+var srcReq = require.context('../../src', true, /\.js$/);
 srcReq.keys().map(srcReq);
 
 // Use webpack to infer and `require` tests automatically.
-const testsReq = require.context('../../src', true, /\.test.js$/);
+var testsReq = require.context('../../src', true, /\.test.js$/);
 testsReq.keys().map(testsReq);
 
-// Only start mocha in browser.
-if (!window.__karma__) {
-  window.mocha.run();
-}
+karmaRunner.startKarma();
