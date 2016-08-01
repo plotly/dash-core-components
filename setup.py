46e68f907dab32a5c1acec496117227916d41337
Per Nilsson <per.nilsson@formidablelabs.com>
2016-07-31 18:33:26 -0700

    Use MANIFEST.in to include JavaScript and metadata

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.2.0',
     author='Chris Parmer',
     author_email='chris@plot.ly',
-    packages=['dash_core_components', 'lib'],
-    package_data={'lib': ['../lib/*']},
+    packages=['dash_core_components'],
+    include_package_data=True,
     license='MIT',
-    description='Dash UI component suite',
+    description='Dash UI core component suite',
     install_requires=['flask', 'plotly', 'flask-cors', 'dash.ly']
 )
