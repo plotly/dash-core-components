487bfbab1078143f577774be5bd5dee20492558d
Tim Head <betatim@gmail.com>
2017-07-24 22:46:51 +0200

    Add basic textarea component

---
+++ src/components/Textarea.react.js
@@ -0,0 +1,95 @@
+import React, {Component, PropTypes} from 'react';
+
+/**
+ * A basic HTML textarea for entering multiline text.
+ *
+ */
+export default class Textarea extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: props.value};
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({value: nextProps.value});
+    }
+
+    render() {
+        const {
+            className,
+            id,
+            fireEvent,
+            placeholder,
+            style,
+            setProps
+        } = this.props;
+        const {value} = this.state;
+
+        return (
+            <textarea
+                className={className}
+                id={id}
+                value={value}
+                placeholder={placeholder}
+                style={style}
+                onChange={e => {
+                    this.setState({value: e.target.value});
+                    if (setProps) setProps({value: e.target.value});
+                    if (fireEvent) fireEvent({event: 'change'});
+                }}
+                onBlur={() => {
+                    if (fireEvent) fireEvent({event: 'blur'});
+                }}
+            />
+        );
+    }
+}
+
+Textarea.propTypes = {
+    id: PropTypes.string,
+    /**
+     * The value of the textarea
+     */
+    value: PropTypes.string,
+
+    /**
+     * A hint to the user of what can be entered in the control.
+     * Note: Do not use the placeholder attribute instead of a Label element,
+     * their purposes are different.
+     * The Label attribute describes the role of the form element
+     * (i.e. it indicates what kind of information is expected),
+     * and the placeholder attribute is a hint about the format
+     * that the value should take.
+     * There are cases in which the placeholder attribute is
+     * never displayed to the user, so the form must be understandable
+     * without it.
+     */
+    placeholder: PropTypes.string,
+
+    /**
+     * The input's inline styles
+     */
+    style: PropTypes.object,
+
+    /**
+     * The class of the input element
+     */
+    className: PropTypes.string,
+
+    /**
+     * If disabled, then the input can not be edited
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * Dash-assigned callback that gets fired when the input changes.
+     */
+    fireEvent: PropTypes.func,
+
+    /**
+     * Dash-assigned callback that gets fired when the value changes.
+     */
+    setProps: PropTypes.func,
+
+    dashEvents: PropTypes.oneOf(['blur', 'change'])
+};
