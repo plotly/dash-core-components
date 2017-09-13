6995bbf82a02ee825a776da7e0b7c363d0c11f13
chriddyp <chris@plot.ly>
2017-09-13 13:59:21 -0400

    Add properties

--- src/components/Upload.react.js
+++ src/components/Upload.react.js
@@ -1,13 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 import Dropzone from 'react-dropzone';
 
-export default class DashDropzone extends Component {
+export default class Upload extends Component {
     constructor() {
         super();
     }
 
     render() {
-        const {setProps} = this.props;
+        const {
+            children,
+            accept,
+            disabled,
+            disable_click,
+            max_size,
+            min_size,
+            multiple,
+            className,
+            className_active,
+            className_reject,
+            className_disabled,
+            style,
+            style_active,
+            style_reject,
+            style_disabled,
+            setProps
+        } = this.props;
         return (
             <Dropzone
                 onDrop={ (files) => {
@@ -18,30 +35,152 @@ export default class DashDropzone extends Component {
                     }
                     reader.readAsBinaryString(file);
                 }}
-                multiple={false}
-                disableClick={this.props.disable_click}
+
+                accept={accept}
+                disabled={disabled}
+                disableClic={disable_click}
+                maxSize={max_size === -1 ? Infinity : max_size}
+                minSize={min_size}
+                multiple={multiple}
+                className={className}
+                activeClassName={className_active}
+                rejectClassName={className_reject}
+                disabledClassName={className_disabled}
+                style={style}
+                activeStyle={style_active}
+                rejectStyle={style_reject}
+                disabledStyle={style_disabled}
             >
-                <div>
-                    <div style={{'textAlign': 'center', 'marginTop': '20px', 'width': '100%'}}>
-                        {'Upload Data File'}
-                    </div>
-                </div>
+                {children}
             </Dropzone>
         );
     }
 }
 
-DashDropzone.propTypes = {
+Upload.propTypes = {
+    /**
+     * ID of the component. Used to identify component
+     * in Dash callback functions.
+     */
     id: PropTypes.string,
 
     /**
-    * Disable clicking on the dropzone to bring up file manager
-    */
+     * The contents of the uploaded file as a binary string
+     */
+    contents: PropTypes.oneOfType([
+        /**
+         * If `multiple` is `false`, then the contents will be a string
+         */
+        PropTypes.string,
+
+        /**
+         * If `multiple` is `true`, then the contents will be an array of strings
+         */
+        PropTypes.arrayOf(PropTypes.string)
+    ]),
+
+    /**
+     * Contents of the upload component
+     */
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.string
+    ]),
+
+    /**
+     * Allow specific types of files.
+     * See https://github.com/okonet/attr-accept for more information.
+     * Keep in mind that mime type determination is not reliable across
+     * platforms. CSV files, for example, are reported as text/plain
+     * under macOS but as application/vnd.ms-excel under Windows.
+     * In some cases there might not be a mime type set at all.
+     * See: https://github.com/react-dropzone/react-dropzone/issues/276
+     */
+    accept: PropTypes.string,
+
+    /**
+     * Enable/disable the upload component entirely
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * Disallow clicking on the component to open the file dialog
+     */
     disable_click: PropTypes.bool,
 
-    contents: PropTypes.string
+    /**
+     * Maximum file size. If `-1`, then infinite
+     */
+    max_size: PropTypes.number,
+
+    /**
+     * Minimum file size
+     */
+    min_size: PropTypes.number,
+
+    /**
+     * Allow dropping multiple files
+     */
+    multiple: PropTypes.bool,
+
+    /**
+     * HTML class name of the component
+     */
+    className: PropTypes.string,
+
+    /**
+     * HTML class name of the component while active
+     */
+    className_active: PropTypes.string,
+
+    /**
+     * HTML class name of the component if rejected
+     */
+    className_reject: PropTypes.string,
+
+    /**
+     * HTML class name of the component if disabled
+     */
+    className_disabled: PropTypes.string,
+
+    /**
+     * CSS styles to apply
+     */
+    style: PropTypes.object,
+
+    /**
+     * CSS styles to apply while active
+     */
+    style_active: PropTypes.object,
+
+    /**
+     * CSS styles if rejected
+     */
+    style_reject: PropTypes.object,
+
+    /**
+     * CSS styles if disabled
+     */
+    style_disabled: PropTypes.object,
+
+    /**
+     * Dash-supplied function for updating props
+     */
+    setProps: PropTypes.func
 };
 
-DashDropzone.defaultProps = {
-    disable_click: false
+Upload.defaultProps = {
+    disabled: false,
+    disable_click: false,
+    max_size: -1,
+    min_size: 0,
+    multiple: false,
+    style: {
+        'width': 200,
+        'height': 200,
+        'border-width': '2px',
+        'border-color': 'rgb(102, 102, 102)',
+        'border-style': 'dashed',
+        'border-radius': '5px'
+    }
 };
