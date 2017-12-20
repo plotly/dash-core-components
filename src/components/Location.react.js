eab38894e5a01d18b36865f71e5ffa318c2395b6
michael <michael@stratodem.com>
2017-12-20 18:48:43 -0500

    Update gitignore for idea, Location.react.js and add package-lock.json

--- src/components/Location.react.js
+++ src/components/Location.react.js
@@ -1,6 +1,9 @@
 import {Component, PropTypes} from 'react';
 /* global window:true */
 
+/**
+ * Update and track the current window.location object through the window.history state
+ */
 export default class Location extends Component {
     constructor(props) {
         super(props);
@@ -8,7 +11,44 @@ export default class Location extends Component {
     }
 
     updateLocation(props) {
-        const {pathname, setProps} = props;
+        const {
+            hash,
+            href,
+            pathname,
+            refresh,
+            search,
+            setProps
+        } = props;
+
+        const propsToSet = {};
+
+        /**
+         * If any of these are not defined, then they were not set by the user
+         * and they need to be equal to the value in window.location.
+         * This only happens on page load (since props will no longer be
+         * undefined after componentDidMount).
+         */
+        // pathname checks
+        if (!pathname)
+            propsToSet.pathname = window.location.pathname;
+        else if (pathname && pathname !== window.location.pathname) {
+            // pathname is defined and not equal to the current window pathname
+            if (refresh) // Refresh the page
+                window.location.pathname = pathname;
+            else
+                propsToSet.pathname = pathname;
+        }
+
+
+        if (!hash && window.location.hash)
+            propsToSet.hash = window.location.hash;
+        if (!href)
+            propsToSet.href = window.location.href;
+        if (!search && window.location.search)
+            propsToSet.search = window.location.search;
+
+
+
 
         /*
          * if pathname isn't defined, then it wasn't set by the user
@@ -18,6 +58,26 @@ export default class Location extends Component {
         if (!pathname) {
             setProps({pathname: window.location.pathname});
         } else if (pathname !== window.location.pathname) {
+            if (props.refresh) {
+                // Refresh the page
+                window.location.pathname = pathname;
+            } else {
+                // Just push the new location into the URL
+                if (setProps) setProps({pathname});
+
+                // Update the state to include pathname and search
+                window.history.pushState({}, '', `${pathname}${search}`);
+            }
+        }
+
+        /*
+         * if search isn't defined, then it wasn't set by the user
+         * and it needs to be equal to the window.location.search
+         * this just happens on page load
+         */
+        if (!search) {
+            setProps({search: window.location.search});
+        } else if (search !== window.location.search) {
             if (props.refresh) {
                 // Refresh the page
                 window.location.pathname = pathname;
@@ -55,8 +115,18 @@ export default class Location extends Component {
 
 Location.propTypes = {
     id: PropTypes.string.isRequired,
+
+    /** pathname in window.location - e.g., "/my/full/pathname" */
     pathname: PropTypes.string,
-    refresh: PropTypes.bool
+    /** search in window.location - e.g., "?myargument=1" */
+    search: PropTypes.string,
+    /** hash in window.location - e.g., "#myhash" */
+    hash: PropTypes.string,
+    /** href in window.location - e.g., "/my/full/pathname?myargument=1#myhash" */
+    href: PropTypes.string,
+
+    /** Refresh the page when the location is updated? */
+    refresh: PropTypes.bool,
 };
 
 Location.defaultProps = {
