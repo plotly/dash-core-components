59a42310bb772ae646f70be3c9ea6474e2b30655
michael <michael@stratodem.com>
2017-12-20 20:05:47 -0500

    Handle hash, href, pathname and search collectively

--- src/components/Location.react.js
+++ src/components/Location.react.js
@@ -23,77 +23,59 @@ export default class Location extends Component {
         const propsToSet = {};
 
         /**
-         * If any of these are not defined, then they were not set by the user
-         * and they need to be equal to the value in window.location.
-         * This only happens on page load (since props will no longer be
-         * undefined after componentDidMount).
+         * Check if the field exists in props. If the prop with "fieldName" is not defined,
+         * then it was not set by the user and needs to be equal to the value in window.location.
+         * This only happens on page load (since props will no longer be undefined after componentDidMount).
+         *
+         * @param {string} fieldName
+         *  The name of the prop in window.location and in the component's prop
+         *
+         * @returns {boolean}
+         *  Returns true if the prop with fieldName is different and the window state needs to be updated
          */
-        // pathname checks
-        if (!pathname)
-            propsToSet.pathname = window.location.pathname;
-        else if (pathname && pathname !== window.location.pathname) {
-            // pathname is defined and not equal to the current window pathname
-            if (refresh) // Refresh the page
-                window.location.pathname = pathname;
-            else
-                propsToSet.pathname = pathname;
-        }
+        const checkExistsUpdateWindowLocation = (fieldName) => {
+            const propVal = propsToSet[fieldName];
 
-
-        if (!hash && window.location.hash)
-            propsToSet.hash = window.location.hash;
-        if (!href)
-            propsToSet.href = window.location.href;
-        if (!search && window.location.search)
-            propsToSet.search = window.location.search;
-
-
-
-
-        /*
-         * if pathname isn't defined, then it wasn't set by the user
-         * and it needs to be equal to the window.location.pathname
-         * this just happens on page load
-         */
-        if (!pathname) {
-            setProps({pathname: window.location.pathname});
-        } else if (pathname !== window.location.pathname) {
-            if (props.refresh) {
-                // Refresh the page
-                window.location.pathname = pathname;
-            } else {
-                // Just push the new location into the URL
-                if (setProps) setProps({pathname});
-
-                // Update the state to include pathname and search
-                window.history.pushState({}, '', `${pathname}${search}`);
+            if (!propVal) // Prop is undefined?
+                propsToSet[fieldName] = window.location[fieldName];
+            else if (propVal !== window.location[fieldName]) { // Prop has changed?
+                if (refresh) // Refresh the page?
+                    window.location[fieldName] = propVal;
+                else {
+                    propsToSet[fieldName] = propVal;
+                    return true;
+                }
             }
-        }
 
-        /*
-         * if search isn't defined, then it wasn't set by the user
-         * and it needs to be equal to the window.location.search
-         * this just happens on page load
-         */
-        if (!search) {
-            setProps({search: window.location.search});
-        } else if (search !== window.location.search) {
-            if (props.refresh) {
-                // Refresh the page
-                window.location.pathname = pathname;
-            } else {
-                // Just push the new location into the URL
-                if (setProps) setProps({pathname});
-                window.history.pushState({}, '', pathname);
-            }
-        }
+            return false;
+        };
+
+        // Check if the prop value needs to be updated (note that this mutates propsToSet)
+        const hrefUpdated = checkExistsUpdateWindowLocation('href');
+        const pathnameUpdated = checkExistsUpdateWindowLocation('pathname');
+        const hashUpdated = checkExistsUpdateWindowLocation('hash');
+        const searchUpdated = checkExistsUpdateWindowLocation('search');
+
+        if (Object.keys(propsToSet).length > 0) // Update the props if there are new props
+            setProps(propsToSet);
+
+        if (hrefUpdated) // Special case -- overrides everything!
+            window.history.pushState({}, '', href);
+        else if (pathnameUpdated || hashUpdated || searchUpdated) // Otherwise, we can mash everything together
+            window.history.pushState({}, '', `${pathname}${search}${hash}`);
     }
 
     componentDidMount() {
         const listener = () => {
             return () => {
                 const {setProps} = this.props;
-                if (setProps) setProps({pathname: window.location.pathname});
+                if (setProps)
+                    setProps({
+                        pathname: window.location.pathname,
+                        href: window.location.href,
+                        hash: window.location.hash,
+                        search: window.location.search
+                    });
             }
         };
         window.addEventListener('onpopstate', listener());
@@ -126,7 +108,7 @@ Location.propTypes = {
     href: PropTypes.string,
 
     /** Refresh the page when the location is updated? */
-    refresh: PropTypes.bool,
+    refresh: PropTypes.bool
 };
 
 Location.defaultProps = {
