98d6a9f82afd24b84f241a966baa7884a3a80be2
John Bampton <jbampton@users.noreply.github.com>
2018-01-05 04:15:45 +1000

    Re-add the two selenium imports

--- test/test_integration.py
+++ test/test_integration.py
@@ -12,6 +12,8 @@ from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_table_experiments as dt
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 try:
     from urlparse import urlparse
