a999bf28ba2a4da14498df28a6e4f225497980da
Carson Sievert <cpsievert1@gmail.com>
2018-04-24 16:22:23 -0500

    more careful checking of wildcards in ...

--- R/components.R
+++ R/components.R
@@ -867,7 +867,7 @@ assert_valid_children <- function(..., wildcards = NULL) {
   kids <- list(...)
   if (length(wildcards)) {
     pattern <- paste(paste0('^', wildcards), collapse = '|')
-    kids <- kids[!grepl(pattern, names(kids))]
+    kids <- kids[!grepl(pattern, names2(kids))]
   }
   if (!length(kids)) return(NULL)
   assert_no_names(kids)
@@ -877,11 +877,16 @@ append_wildcard_props <- function(component, wildcards = NULL, ...) {
   attrs <- list(...)
   if (!length(attrs) || !length(wildcards)) return(component)
   pattern <- paste(paste0('^', wildcards), collapse = '|')
-  wildcards <- attrs[grepl(pattern, names(attrs))]
-  component[['props']] <- c(component[['props']], wildcards)
-  component[['propNames']] <- c(component[['propNames']], names(wildcards))
+  attrs_wild <- attrs[grepl(pattern, names2(attrs))]
+  if (!length(attrs_wild)) return(component)
+  component[['props']] <- c(component[['props']] %||% list(), attrs_wild)
+  component[['propNames']] <- c(component[['propNames']], names(attrs_wild))
   component
 }
+
+names2 <- function(x) names(x) %||% rep('', length(x))
+
+`%||%` <- function(x, y) if (length(x)) x else y
 
 assert_no_names <- function(x) {
   nms <- names(x)
