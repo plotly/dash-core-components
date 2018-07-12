b6e5f83fc2be798de0c1987624c3ef3a9a527628
Valentijn Nieman <valentijnnieman@gmail.com>
2018-07-12 11:55:49 +0200

    Removed useless percy screenshot from link test

--- test/test_integration.py
+++ test/test_integration.py
@@ -525,14 +525,6 @@ class Tests(IntegrationTests):
             2
         )
 
-        # test if link correctly scrolls back to top of page
-        test_link = self.wait_for_element_by_css_selector('#test-link')
-        test_link.send_keys(Keys.NULL)
-        self.snapshot('link on bottom of page - initial')
-        test_link.click()
-        time.sleep(2)
-        self.snapshot('link on bottom of page - after clicking (should be scrolled back to top)')
-
         # test link still fires update on Location
         page_content = self.wait_for_element_by_css_selector('#page-content')
         self.assertNotEqual(page_content.text, 'You are on page /')
