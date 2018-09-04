afedf2200f235c93ceec5c1b74b0ed8fb1555518
Philippe Duval <philippe@plot.ly>
2018-09-04 17:39:11 -0400

    Set devtool eval-source-map for dev.

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,40 @@ const packagejson = require('./package.json');
 
 const dashLibraryName = packagejson.name.replace(/-/g, '_');
 
-module.exports = (env, argv) => {
-    return {
-        entry: {main: './src/index.js'},
-        output: {
-            path: path.resolve(__dirname, dashLibraryName),
-            filename: argv.mode === 'development' ? 'bundle.dev.js' : 'bundle.js',
-            library: dashLibraryName,
-            libraryTarget: 'window'
-        },
-        externals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-            'plotly.js': 'Plotly'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader'
-                        },
-                        {
-                            loader: 'css-loader'
-                        }
-                    ]
+module.exports = (env, argv) => ({
+    entry: {main: './src/index.js'},
+    output: {
+        path: path.resolve(__dirname, dashLibraryName),
+        filename: argv.mode === 'development' ? 'bundle.dev.js' : 'bundle.js',
+        library: dashLibraryName,
+        libraryTarget: 'window'
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'plotly.js': 'Plotly'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
                 }
-            ]
-        }
-    };
-};
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    devtool: argv.mode === 'development' ? 'eval-source-map' : 'none'
+});
