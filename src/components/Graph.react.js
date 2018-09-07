f90fe4cd825b1d973b289400b9b711c9093d7f7b
Valentijn Nieman <valentijnnieman@gmail.com>
2018-09-07 12:53:24 -0400

    ESlint wants a space

--- src/components/Graph.react.js
+++ src/components/Graph.react.js
@@ -458,9 +458,9 @@ PlotlyGraph.propTypes = {
 }
 
 PlotlyGraph.defaultProps = {
-    /*eslint-disable no-magic-numbers*/
+    /* eslint-disable no-magic-numbers */
     id: 'graph-' + Math.random().toString(36).substring(2,7),
-    /*eslint-enable no-magic-numbers*/
+    /* eslint-enable no-magic-numbers */
     clickData: null,
     hoverData: null,
     selectedData: null,
