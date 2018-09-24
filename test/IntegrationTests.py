100540d9a7e29399ab952ca2c5c45836eadf4f87
Chris P <chris@plot.ly>
2018-09-24 08:56:34 -0400

    :see_no_evil: try another configuration

--- test/IntegrationTests.py
+++ test/IntegrationTests.py
@@ -29,7 +29,8 @@ class IntegrationTests(unittest.TestCase):
         root_static_dir = os.path.abspath(
             os.path.join(
                 os.path.dirname(__file__),
-                '..'
+                '..',
+                'dash_core_components'
             )
         )
         loader = percy.ResourceLoader(
