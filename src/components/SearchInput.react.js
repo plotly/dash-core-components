ac45621117f0c9366dc6fb0143cdcd01d42ff4c9
Philippe Duval <philippe@plot.ly>
2018-10-04 13:29:41 -0400

    Add more props.

--- src/components/SearchInput.react.js
+++ src/components/SearchInput.react.js
@@ -18,20 +18,46 @@ export default class SearchInput extends React.Component {
                 value: e.target.value,
             });
         }
-        return true;
     }
 
     render() {
-        const {id, className, style, search_data} = this.props;
+        const {
+            id,
+            className,
+            style,
+            search_data,
+            single_line,
+            allow_space_in_query,
+            markup,
+            suggestion_portal_host_id,
+            mentions_style,
+            mentions_className,
+        } = this.props;
+
+        let sug = null;
+        if (suggestion_portal_host_id) {
+            sug = document.getElementById(suggestion_portal_host_id);
+        }
 
         return (
-            <div id={id} className={className} style={style}>
+            <div id={id}>
                 <MentionsInput
                     value={this.state.value}
                     onChange={this.onChange}
+                    singleLine={single_line}
+                    className={className}
+                    style={style}
+                    markup={markup}
+                    suggestionsPortalHost={sug}
+                    allowSpaceInQuery={allow_space_in_query}
                 >
                     {search_data.map(e => (
-                        <Mention trigger={e.trigger} data={e.options} />
+                        <Mention
+                            trigger={e.trigger}
+                            data={e.options}
+                            style={e.style || mentions_style}
+                            className={e.className || mentions_className}
+                        />
                     ))}
                 </MentionsInput>
             </div>
@@ -41,18 +67,84 @@ export default class SearchInput extends React.Component {
 
 SearchInput.defaultProps = {
     value: '',
+    single_line: false,
+    allow_space_in_query: false,
 };
 
 SearchInput.propTypes = {
     id: PropTypes.string,
+
+    /**
+     * Class name for the top div container.
+     */
     className: PropTypes.string,
+    /**
+     * Style for the top div container.
+     */
     style: PropTypes.object,
+
+    /**
+     * Options for mentions.
+     */
     search_data: PropTypes.arrayOf(
         PropTypes.shape({
+            /**
+             * The symbol to open the suggestions.
+             */
             trigger: PropTypes.string,
-            options: PropTypes.string,
+            /**
+             * Options to suggest when the symbol is typed.
+             */
+            options: PropTypes.arrayOf(
+                PropTypes.shape({
+                    id: PropTypes.string,
+                    display: PropTypes.string,
+                })
+            ),
+            /**
+             * Style of the suggestions for this trigger.
+             */
+            style: PropTypes.object,
+            /**
+             * ClassName for a suggestion.
+             */
+            className: PropTypes.string
         })
     ),
+
+    /**
+     * The current value of the input.
+     */
     value: PropTypes.string,
+
+    /**
+     * Render <input> instead of <textarea>
+     */
+    single_line: PropTypes.bool,
+    /**
+     * Suggestions stays open even if the user separate words with spaces.
+     */
+    allow_space_in_query: PropTypes.bool,
+
+    /**
+     * Id of a component to render the suggestions into.
+     */
+    suggestion_portal_host_id: PropTypes.string,
+
+    /**
+     * Template to use when rendering the options
+     * default: @[__display__](__id__)
+     */
+    markup: PropTypes.string,
+
+    /**
+     * Global style to give to the wrapped <Mention/>'s
+     */
+    mentions_style: PropTypes.object,
+    /**
+     * Global css class to give to every <Mention/> component.
+     */
+    mentions_className: PropTypes.string,
+
     setProps: PropTypes.func,
 };
