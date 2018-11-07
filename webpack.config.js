a751cd28a7c1ea5d0f87326a0361322a7b5ae2d7
Valentijn Nieman <valentijnnieman@gmail.com>
2018-11-07 11:49:45 -0500

    Update webpack to parse .jsx, update extract-meta to not parse .jsx

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = (env, argv) => {
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                    test: /\.jsx?$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
