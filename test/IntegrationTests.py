b89139ec0c783e9d3f74dd91b87c2503a4a91d26
Valentijn Nieman <valentijnnieman@gmail.com>
2018-11-19 15:35:42 -0500

    Try relative root_dir

--- test/IntegrationTests.py
+++ test/IntegrationTests.py
@@ -25,14 +25,10 @@ class IntegrationTests(unittest.TestCase):
             options.binary_location = os.environ['DASH_TEST_CHROMEPATH']
 
         cls.driver = webdriver.Chrome(chrome_options=options)
-        # Build a ResourceLoader that knows how to collect assets for this application.
-        root_static_dir = os.path.join(os.path.dirname(__file__), 'test/assets')
         loader = percy.ResourceLoader(
-            root_dir=root_static_dir,
-            # Prepend `/assets` to all of the files in the static directory, to match production assets.
-            # This is only needed if your static assets are served from a nested directory.
-            base_url='/assets',
             webdriver=cls.driver,
+            base_url='/assets',
+            root_dir='test/assets'
         )
         cls.percy_runner = percy.Runner(loader=loader)
         cls.percy_runner.initialize_build()
