568adac6dad0d3c6da25e86a553b7b2175f0abb7
t4rk <t4rk@outlook.com>
2019-03-18 13:43:10 -0400

    :hocho: Remove dash import

--- dash_core_components/__init__.py
+++ dash_core_components/__init__.py
@@ -4,24 +4,8 @@ from __future__ import print_function as _
 
 import os as _os
 import sys as _sys
-import dash as _dash
 
 from .version import __version__
-
-# Module imports trigger a dash.development import, need to check this first
-if not hasattr(_dash, 'development'):
-    print("Dash was not successfully imported. Make sure you don't have a file "
-          "named \n'dash.py' in your current directory.", file=_sys.stderr)
-    _sys.exit(1)
-
-# Must update to dash>=0.23.1 to use this version of dash-core-components
-if not hasattr(_dash.development.base_component, '_explicitize_args'):
-    print("Please update the `dash` module to >= 0.23.1 to use this "
-          "version of dash_core_components.\n"
-          "You are using version {:s}".format(_dash.version.__version__),
-          file=_sys.stderr)
-    _sys.exit(1)
-
 
 from ._imports_ import *
 from ._imports_ import __all__
