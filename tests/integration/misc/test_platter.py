ca8e38a065d58b9bae2c28950985091903df639a
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-09-23 11:02:09 -0400

    dash_duo

--- tests/integration/misc/test_platter.py
+++ tests/integration/misc/test_platter.py
@@ -12,9 +12,7 @@ def test_mspl001_dcc_components_platter(platter_app, dash_duo):
     dash_duo.wait_for_element("#waitfor")
 
     # wait for Graph to be ready
-    WebDriverWait(self.driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#graph .main-svg"))
-    )
+    dash_duo.wait_for_element()"#graph .main-svg")
 
     dash_duo.percy_snapshot("gallery")
 
