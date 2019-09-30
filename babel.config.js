a8c8cb2281784162fb993bcca40c01c2e1e55d03
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-09-30 11:57:33 -0400

    test config

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,14 @@ const presets = [
 ];
 
 const plugins = [
-    '@babel/plugin-syntax-dynamic-import',
-    "styled-jsx/babel"
+    '@babel/plugin-syntax-dynamic-import'
 ];
+
+// eslint-disable-next-line no-process-env
+if (process.env.ENV === "test") {
+    plugins.push("styled-jsx/babel-test");
+} else {
+    plugins.push("styled-jsx/babel");
+}
 
 module.exports = { presets, plugins };
