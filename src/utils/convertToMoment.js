bbd3b76501895cb6eac07bc2bb2dc194807a8fe0
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-10-28 17:17:42 -0400

    Use max date allowed

--- src/utils/convertToMoment.js
+++ src/utils/convertToMoment.js
@@ -11,8 +11,10 @@ export default (newProps, momentProps) => {
             dest[key] = null;
 
             if (key === 'initial_visible_month') {
-                dest[key] = newProps['start_date'] || newProps['min_date_allowed'] ?
-		    moment(newProps['start_date'] || newProps['min_date_allowed']) :
+                dest[key] = newProps['start_date'] || newProps['min_date_allowed']
+		    || newProps['end_date'] || newProps['max_date_allowed'] ?
+		    moment(newProps['start_date'] || newProps['min_date_allowed']
+			  || newProps['end_date'] || newProps['max_date_allowed']) :
 		    moment();
             }
         } else {
