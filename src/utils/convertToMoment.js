c1bbde2ecbc01252fa9034944779ce7438cd8d7f
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-11-05 11:18:28 -0500

    Run linter.

--- src/utils/convertToMoment.js
+++ src/utils/convertToMoment.js
@@ -11,8 +11,12 @@ export default (newProps, momentProps) => {
             dest[key] = null;
 
             if (key === 'initial_visible_month') {
-                dest[key] = newProps['start_date'] || newProps['min_date_allowed']
-		    || newProps['end_date'] || newProps['max_date_allowed'] || undefined;
+                dest[key] =
+                    newProps.start_date ||
+                    newProps.min_date_allowed ||
+                    newProps.end_date ||
+                    newProps.max_date_allowed ||
+                    undefined;
             }
         } else {
             dest[key] = moment(value);
