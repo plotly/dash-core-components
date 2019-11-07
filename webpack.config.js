47586159805473ad1e24a2e0d626d80d4513a7fb
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-11-07 09:53:19 -0500

    update exclude, update dynamic plugin

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.jsx?$/,
-                    exclude: /node_modules/,
+                    exclude: /node_modules\/(?!@plotly\/dash-component-plugins)/,
                     use: {
                         loader: 'babel-loader',
                     },
