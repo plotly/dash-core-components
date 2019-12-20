a2ba931874252bfa81223d565c9525bd18917496
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-12-20 10:15:29 -0500

    Use click to trigger md render in test.

--- tests/integration/misc/test_markdown_highlight.py
+++ tests/integration/misc/test_markdown_highlight.py
@@ -24,7 +24,7 @@ def test_msmh001_no_window_variable(dash_dcc):
 def test_msmh002_window_override(dash_dcc):
     app = dash.Dash(__name__)
     app.layout = html.Div([
-        html.Div(id='md-trigger'),
+        html.Button(id='md-trigger'),
         html.Div(id='md-container')
     ])
 
@@ -32,14 +32,17 @@ def test_msmh002_window_override(dash_dcc):
     # so we need to trigger a rerender of the markdown component
     @app.callback(
         Output('md-container', 'children'),
-        [Input('md-trigger', 'children')]
+        [Input('md-trigger', 'n_clicks')]
     )
-    def trigger_md_rerender(_):
-        return dcc.Markdown(md_text)
+    def trigger_md_rerender(nclicks):
+        if nclicks is not None and nclicks > 0:
+            return dcc.Markdown(md_text)
+        return None
 
     dash_dcc.start_server(app)
 
     dash_dcc.driver.execute_script('window.hljs = {highlightBlock: (block) => {block.innerHTML="hljs override"}};')
 
+    dash_dcc.find_element('#md-trigger').click()
     dash_dcc.wait_for_text_to_equal('#md-container code', 'hljs override')
     dash_dcc.percy_snapshot('md_code_highlight_override')
