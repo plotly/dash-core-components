5cdd2c0afe175a7099ad03ad974b8cd34b3c07ad
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-01-21 15:59:41 -0500

    bump rc-slider, remove base style

--- src/components/Slider.react.js
+++ src/components/Slider.react.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import ReactSlider, {createSliderWithTooltip} from 'rc-slider';
 import PropTypes from 'prop-types';
 import {assoc, omit, pickBy} from 'ramda';
-import './css/rc-slider@6.1.2.css';
 import computeSliderStyle from '../utils/computeSliderStyle';
 
 /**
