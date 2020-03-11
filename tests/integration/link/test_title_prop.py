31720e7e6223225990c93db92a83c6261575c003
hammadtheone <hammadtheone@gmail.com>
2020-03-11 15:55:03 -0400

    Adding basic test

---
+++ tests/integration/link/test_title_prop.py
@@ -0,0 +1,32 @@
+import pytest
+import dash
+from dash.dependencies import Input, Output, State
+import dash.testing.wait as wait
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+@pytest.mark.DCC768
+def test_title001_prop(dash_dcc):
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        [
+            dcc.Link("page 1", id ="link1", href="/page-1", title="This is a test title!"),
+            dcc.Location(id="url", refresh=False),
+            html.Div(id="content")
+        ]
+    )
+    @app.callback(Output("content", "children"), [Input("link1", "title")])
+    def display_title(title):
+        return title
+
+    dash_dcc.start_server(app)
+
+    title_exists = dash_dcc.driver.execute_script(
+        '''
+        return document.getElementById("link1").hasAttribute("title");
+        '''
+    )
+
+    assert title_exists
+
