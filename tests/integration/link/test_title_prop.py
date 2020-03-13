804da4af0eb3db0689d5f33e8c1a6f6a45b91f95
hammadtheone <hammadtheone@gmail.com>
2020-03-12 23:54:20 -0400

    Fixed integration test

--- tests/integration/link/test_title_prop.py
+++ tests/integration/link/test_title_prop.py
@@ -1,7 +1,6 @@
 import pytest
 import dash
-from dash.dependencies import Input, Output, State
-import dash.testing.wait as wait
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
@@ -11,7 +10,7 @@ def test_title001_prop(dash_dcc):
     app = dash.Dash(__name__)
     app.layout = html.Div(
         [
-            dcc.Link("page 1", id ="link1", href="/page-1", title="This is a test title!"),
+            dcc.Link("page 1", id="link1", href="/page-1", title="This is a test title!"),
             dcc.Location(id="url", refresh=False),
             html.Div(id="content")
         ]
@@ -28,5 +27,4 @@ def test_title001_prop(dash_dcc):
         '''
     )
 
-    assert "This is a test title!"
-
+    assert title_exists == "This is a test title!"
