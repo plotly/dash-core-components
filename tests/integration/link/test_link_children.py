5eac66e752b3e455a3384239dfe57ebae9cdf92b
hammadtheone <hammadtheone@gmail.com>
2020-03-17 14:13:46 -0400

    Added test for children

--- tests/integration/link/test_link_children.py
+++ tests/integration/link/test_link_children.py
@@ -15,10 +15,24 @@ def test_lich001_default(dash_dcc):
             html.Div(id="content")
         ]
     )
+    dash_dcc.start_server(app)
+
+    dash_dcc.wait_for_text_to_equal("#link1", "/page-1")
+
+@pytest.mark.DCC776
+def test_lich002_children(dash_dcc):
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        [
+            dcc.Link(children='test children', id="link1", href="/page-1"),
+            dcc.Location(id="url", refresh=False),
+            html.Div(id="content")
+        ]
+    )
     @app.callback(Output("content", "children"), [Input("link1", "children")])
     def display_children(children):
         return children
 
     dash_dcc.start_server(app)
 
-    dash_dcc.wait_for_text_to_equal("#link1", "/page-1")
+    dash_dcc.wait_for_text_to_equal("#content", "test children")
