745c4adc4bab00db5a17f3b34381bd889eaf8968
emher <emil.h.eriksen@gmail.com>
2020-09-12 21:39:24 +0200

    Added integration tests and send_X utils methods, for now residing in express.py

---
+++ tests/integration/download/test_download.py
@@ -0,0 +1,35 @@
+import os
+import time
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+from dash.dependencies import Input, Output
+
+
+def test_download_text(dash_dcc):
+    text = "Hello, world!"
+    filename = "hello.txt"
+    # Create app.
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        [
+            dcc.Store(id="content", data=text),
+            dcc.Download(id="download"),
+        ]
+    )
+
+    @app.callback(Output("download", "data"), [Input("content", "data")])
+    def download(content):
+        return dict(filename=filename, content=content)
+
+    # Check that there is nothing before starting the app
+    fp = os.path.join(dash_dcc.download_path, filename)
+    assert not os.path.isfile(fp)
+    # Run the app.
+    dash_dcc.start_server(app)
+    time.sleep(0.5)
+    # Check that a file has been download, and that it's content matches the original text.
+    with open(fp, 'r') as f:
+        content = f.read()
+    assert content == text
---
+++ tests/integration/download/test_download_file.py
@@ -0,0 +1,38 @@
+import os
+import time
+import dash
+import dash_core_components as dcc
+import dash_core_components.express as dcx
+import dash_html_components as html
+
+from dash.dependencies import Input, Output
+
+
+def test_download_file(dash_dcc):
+    filename = "Lenna.jpeg"
+    asset_folder = "./download-assets"
+    # Create app.
+    app = dash.Dash(__name__)
+    app.layout = html.Div(
+        [
+            dcc.Input(id="input", value=filename),
+            dcc.Download(id="download"),
+        ]
+    )
+
+    @app.callback(Output("download", "data"), [Input("input", "value")])
+    def download(value):
+        return dcx.send_file(os.path.join(asset_folder, value))
+
+    # Check that there is nothing before starting the app
+    fp = os.path.join(dash_dcc.download_path, filename)
+    assert not os.path.isfile(fp)
+    # Run the app.
+    dash_dcc.start_server(app)
+    time.sleep(0.5)
+    # Check that a file has been download, and that it's content matches the original.
+    with open(fp, 'rb') as f:
+        content = f.read()
+    with open(os.path.join(asset_folder, filename), 'rb') as f:
+        original = f.read()
+    assert content == original
