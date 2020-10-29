91a424e73d5b4592265175b35dada98dc6d88182
Marc-André Rivet <marc-andre.rivet@plot.ly>
2020-10-29 16:27:35 -0400

    Update missing Julia artifact

--- lib/DashCoreComponents.jl
+++ lib/DashCoreComponents.jl
@@ -3,7 +3,7 @@ module DashCoreComponents
 using DashBase
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "1.12.1"
+const version = "1.13.0"
 
 include("dcc_checklist.jl")
 include("dcc_confirmdialog.jl")
@@ -37,147 +37,147 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "async-datepicker.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-datepicker.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-datepicker.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-dropdown.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-dropdown.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-dropdown.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-graph.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-graph.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-graph.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-highlight.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-highlight.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-highlight.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-markdown.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-markdown.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-markdown.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-slider.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-slider.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-slider.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-upload.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-upload.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-upload.js",
     dynamic = nothing,
     async = :true,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-datepicker.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-datepicker.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-datepicker.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-dropdown.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-dropdown.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-dropdown.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-graph.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-graph.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-graph.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-highlight.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-highlight.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-highlight.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-markdown.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-markdown.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-markdown.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-slider.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-slider.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-slider.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-upload.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-upload.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-upload.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_core_components.min.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/dash_core_components.min.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/dash_core_components.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_core_components.min.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/dash_core_components.min.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/dash_core_components.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_core_components-shared.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/dash_core_components-shared.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/dash_core_components-shared.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_core_components-shared.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/dash_core_components-shared.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/dash_core_components-shared.js.map",
     dynamic = true,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "plotly.min.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/plotly.min.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/plotly.min.js",
     dynamic = nothing,
     async = :eager,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-plotlyjs.js",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-plotlyjs.js",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-plotlyjs.js",
     dynamic = nothing,
     async = :lazy,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "async-plotlyjs.js.map",
-    external_url = "https://unpkg.com/dash-core-components@1.12.1/dash_core_components/async-plotlyjs.js.map",
+    external_url = "https://unpkg.com/dash-core-components@1.13.0/dash_core_components/async-plotlyjs.js.map",
     dynamic = true,
     async = nothing,
     type = :js
