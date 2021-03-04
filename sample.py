d57bf70199c5f8320134643afbde92c92be3572d
Joseph Damiba <joseph.damiba@plot.ly>
2021-03-04 07:48:05 -0500

    lint

--- sample.py
+++ sample.py
@@ -2,20 +2,17 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    dcc.Slider(
-        id='my-slider',
-        min=0,
-        syncedInput=True,
-        max=20,
-        step=0.5,
-        value=10,
-    ),
-    html.Div(id='slider-output-container')
-])
+app.layout = html.Div(
+    [
+        dcc.Slider(
+            id="my-slider", min=0, syncedInput=True, max=20, step=0.5, value=10,
+        ),
+        html.Div(id="slider-output-container"),
+    ]
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
