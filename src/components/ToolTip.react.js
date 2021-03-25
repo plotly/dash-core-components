cdbdf1967ea5b9e41abd33e43b32c0f07e139076
Ricky Reusser <rsreusser@gmail.com>
2021-03-25 12:00:13 -0700

    Remove JSXStyle workaround

--- src/components/ToolTip.react.js
+++ src/components/ToolTip.react.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {omit} from 'ramda';
-import _JSXStyle from "styled-jsx/style";
 
 /**
  * A basic HTML textarea for entering multiline text.
