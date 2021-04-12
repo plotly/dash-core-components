2697a705c049ba9f24172591e4a1d87e1f53c8bc
Ann Marie Ward <amward@fastmail.us>
2021-04-12 12:57:19 -0700

    fixed tests

--- tests/integration/clipboard/test_clipboard.py
+++ tests/integration/clipboard/test_clipboard.py
@@ -1,48 +1,59 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash.testing.wait as wait
+import time
+
 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 
-def test_clp001_clipboard_text(dash_dcc):
+def test_clp001_clipboard_text(dash_dcc_headed):
     copy_text = "Hello, Dash!"
     app = dash.Dash(__name__, prevent_initial_callbacks=True)
     app.layout = html.Div(
         [
-            html.Div(copy_text, id="copy"),
-            dcc.Clipboard(id="copy_icon", target_id="copy"),
-            dcc.Textarea(id="paste"),
+            html.Div(copy_text, id="copy1"),
+            dcc.Clipboard(id="copy_icon1", target_id="copy1"),
+            dcc.Textarea(id="paste1"),
         ]
     )
-    dash_dcc.start_server(app)
+    dash_dcc_headed.start_server(app)
+    dash_dcc_headed.driver.execute_script("navigator.clipboard.writeText('')")
 
-    dash_dcc.find_element("#copy_icon").click()
-    dash_dcc.find_element("#paste").click()
-    ActionChains(dash_dcc.driver).key_down(Keys.CONTROL).send_keys("v").key_up(
+    dash_dcc_headed.find_element("#copy_icon1").click()
+    time.sleep(2)
+    dash_dcc_headed.find_element("#paste1").click()
+    ActionChains(dash_dcc_headed.driver).key_down(Keys.CONTROL).send_keys("v").key_up(
         Keys.CONTROL
     ).perform()
 
-    elem = dash_dcc.find_element("#paste")
-    val = elem.get_attribute("value")
-    assert val == copy_text
+    wait.until(
+        lambda: dash_dcc_headed.find_element("#paste1").get_attribute("value")
+        == copy_text,
+        timeout=3,
+    )
 
 
-def test_clp002_clipboard_text(dash_dcc):
-    copy_text = "Copy"
+def test_clp002_clipboard_text(dash_dcc_headed):
+    copy_text = "Copy this text to the clipboard"
     app = dash.Dash(__name__, prevent_initial_callbacks=True)
     app.layout = html.Div(
-        [dcc.Clipboard(id="copy_icon", text=copy_text), dcc.Textarea(id="paste")]
+        [dcc.Clipboard(id="copy_icon2", text=copy_text), dcc.Textarea(id="paste2")]
     )
-    dash_dcc.start_server(app)
+    dash_dcc_headed.start_server(app)
+    dash_dcc_headed.driver.execute_script("navigator.clipboard.writeText('')")
 
-    dash_dcc.find_element("#copy_icon").click()
-    dash_dcc.find_element("#paste").click()
-    ActionChains(dash_dcc.driver).key_down(Keys.CONTROL).send_keys("v").key_up(
+    dash_dcc_headed.find_element("#copy_icon2").click()
+    time.sleep(2)
+    dash_dcc_headed.find_element("#paste2").click()
+    ActionChains(dash_dcc_headed.driver).key_down(Keys.CONTROL).send_keys("v").key_up(
         Keys.CONTROL
     ).perform()
 
-    elem = dash_dcc.find_element("#paste")
-    val = elem.get_attribute("value")
-    assert val == copy_text
+    wait.until(
+        lambda: dash_dcc_headed.find_element("#paste2").get_attribute("value")
+        == copy_text,
+        timeout=3,
+    )
