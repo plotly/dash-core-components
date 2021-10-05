c68b7eb61dd919955b7ac23b950bedc44a87786b
Alexandr Romanenko <waralex@gmail.com>
2021-10-05 10:38:45 +0300

    Empty Julia Package

---
+++ src/DashCoreComponents.jl
@@ -0,0 +1,5 @@
+module DashCoreComponents
+    function __init__()
+        @warn "`using DashCoreComponents` is deprecated, all DashCoreComponents functions have been moved to Dash package. Use `using Dash` instead"
+    end
+end
